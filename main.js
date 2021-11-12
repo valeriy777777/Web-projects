d7addaa9611f55ffdee9eb4d6c4c612b15c6ccef
valeriy777777 <83246563+valeriy777777@users.noreply.github.com>
2021-11-12 18:25:25 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
+Vue.config.productionTip = false
 
-
-import { diffDates, diffToHtml } from "./datecalc.js";
-import { formatError } from "./utils.js";
-
-import { toggleShow } from "./toggleshow.js";
-import { runCountdown } from "./countdown.js";
-
-
-
-const dateCalcForm = document.getElementById("datecalc");
-const dateCalcResult = document.getElementById("datecalc__result");
-function handleCalcDates(event) {
-    dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля");
-}
-
-
-
-
-const countdownDiv = document.querySelector('#countdown');
-countdownDiv.style.display = 'none';
-
-toggleShow(dateCalcForm, countdownDiv);
-
-const deadline = '2021-10-31';
-
-runCountdown('#countdown', deadline);
+new Vue({
+  el: '#app',
+  template: '<App />',
+  components: {
+    App,
+  }
+})
